736142a62df64323788e5d6c64b74dfbdc1194bb
LFL <dragonli_52171@163.com>
2016-05-29 19:03:43 +0800

    04.Functions,enum,05.closures

---
+++ BaseSwift2.2Code/04.Functions,enum.playground/Contents.swift
@@ -0,0 +1,170 @@
+
+//16.05.29 下午
+import UIKit
+
+//函数名,形参类型 返回值类型
+
+func helloSwift(name:String) -> String {
+    return "\(name)"
+}
+
+helloSwift("DragonLi")
+
+func opterion(num:Int ,num2:Int ) -> Int {
+    return num + num2
+}
+
+opterion(7, num2: 5) // 第二个开始形参不可以去掉
+
+//多个返回值 // 返回元祖
+func square(num:Double) -> (posSqrt:Double,negSqtr:Double) {
+    let posSqrt = sqrt(fabs(num))  // 正的平方根
+    print(posSqrt)
+    let negSqtr = -sqrt(fabs(num))  // 负的平方根
+    return(posSqrt,negSqtr)
+}
+
+let (x,y) = square(3)
+
+//指定函数形参名字
+func test1(name name:String){
+    print(name)
+}
+
+test1(name: "DragonLi")
+
+// 如果函数参数和返回都一样,使用inout 简写
+func swapNnmber ( inout num1:Int ,inout num2:Int){
+    let tempValue = num1
+    num1 = num2
+    num2 = tempValue
+}
+//就需要传递地址
+var a = 10
+var b = 15
+swap(&a, &b)
+a
+b
+
+//1.如果参数类型不确定...    Eg 求平均数
+func average(num:Int...) -> Int {
+    var total  = 0
+    for value in num {
+      total += value
+    }
+    return total / (num.count)
+}
+
+average(10,12,13,18)
+
+/*
+ 函数类型,swift中函数和其他值类型一样,可以赋值,作为函数参数和返回值返回
+ 函数类型===== 参数和返回值类型确定
+*/
+
+func addNumber(num1:Int ,num2:Int) -> Int {
+    return num2 + num1
+}
+
+func multiplyNumber(num1:Int ,num2:Int) -> Int {
+    return num2 * num1
+}
+//上面二个就是函数类型相同的 (Int ,Int) ->Int
+
+//,可以直接赋值
+var mathFunc = addNumber
+mathFunc.dynamicType
+ mathFunc(1,num2: 3)
+
+//进阶\
+
+func add(input:Int) -> Int {
+    return input + 1
+}
+
+func dive(input:Int) -> Int {
+    return input - 1
+}
+
+func chooseFunc(what:Bool) -> (Int) ->Int {
+    return what ? add : dive
+}
+//调用后返回对应函数
+let resultFunc =  chooseFunc(true)
+
+resultFunc(2)
+
+//对于返回值可以类似于OC的typedef  
+typealias backType = (Int) ->Int // 替换98行返回即可
+
+//  嵌套函数:定义在函数内,外界可以使用,
+
+
+func chooseFunc1(what:Bool) -> backType{
+    func add2(input:Int) -> Int {
+        return input + 1
+    }
+    
+    func dive2(input:Int) -> Int {
+        return input - 1
+    }
+   return  what ? add2 : dive2
+}
+
+let funChooseWhat = chooseFunc1(true)
+funChooseWhat(2)   // 3
+
+
+
+
+//      枚举 和OC不同,默认没有初始值的
+enum frame{
+    case x
+    case y
+    case width
+    case height
+}
+
+var frameDir = frame.y
+    frameDir.dynamicType
+
+frameDir = .width  // 已知类型
+
+    switch frameDir{
+    case .x :
+        print(frameDir)
+        
+    default: break // 不想列举所有成员值,加上
+    }
+
+//实际项目中使用
+enum shape{
+    case rectagle(width:Int,height:Int)
+    case round (radius:Int)
+}
+
+var square = shape.rectagle(width: 10, height: 10)
+
+//指定类型
+enum frameValue :Int{
+    case x
+    case y
+    case width
+    case height
+}
+
+frameValue.width.rawValue  // Int 才有,如果是String 就是String
+var valueTest = frameValue(rawValue:2) // 没有提示的
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ BaseSwift2.2Code/05.closures.playground/Contents.swift
@@ -0,0 +1,94 @@
+
+import UIKit
+
+//  闭包 :自包含的函数代码块,函数是闭包的一种类型
+//1.闭包表达式语法
+/*
+ {
+ (parameters) ->return Type  in
+ code
+ }
+ */
+//类似于函数,可以理解为匿名函数,(内联闭包)
+
+func caculate(a:Int,_ b:Int, _ operation : (Int,Int) ->Int) -> Int {
+    return operation(a,b)
+}
+
+func plus(a:Int,b:Int) -> Int {
+    return a + b
+}
+
+func mulitly(a:Int,b:Int) -> Int {
+    return a * b
+}
+
+caculate(3, 7, mulitly)  // 函数类型的使用,-----闭包实现最后参数?
+
+//  
+let plusClosure = {
+    (a:Int,b:Int)   ->Int in
+    return a + b
+}
+//查看类型 ,和函数一样
+plusClosure.dynamicType
+
+caculate(2, 10, plusClosure)
+
+let mulityClousure = {
+    (a:Int ,b:Int) ->Int in
+    return a * b
+}
+
+caculate(3, 6, mulityClousure)
+
+// 上面闭包和写函数一致,匿名呢
+caculate(5, 5, {  (a:Int ,b:Int) ->Int in return a * b })
+
+//因为swift 类型推断,闭包特性:根据上下文推断类型,简写
+caculate(5, 5, {  (a ,b) in  a * b })
+
+// 默认 $1,$2
+caculate(5, 5, {$0 + $1})
+
+//运算符函数 ,少到没朋友
+caculate(5, 5,  + )
+
+//2.尾随闭包
+caculate(5, 5){$0 + $1}
+
+//捕获值
+
+func increasefun(num:Int) -> () ->Int {
+    var number = 0
+    func funDetail () ->Int{
+        number += num
+        return number
+    }
+    return funDetail
+}
+
+let numberWhat = increasefun(20)
+//每次调用都会把内部的number 增加
+numberWhat()  // 20
+numberWhat()  // 40
+
+let numberWhat2 = increasefun(10)
+//每次调用都会把内部的number 增加
+numberWhat2()  // 10
+numberWhat2()  // 20
+
+// 声明不同,调用不同,捕获值不同,引用类型
+//每次都是产生新的副本
+
+let numberWhat3 = numberWhat2
+//每次调用都会把内部的number 增加
+numberWhat3()  // 20 + 10
+
+
+
+
+
+
+
+
